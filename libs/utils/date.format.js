27a0624c54d0ea2ba69e049a8b707ec2a6281a61
Aman Sultan Baig <amansultanbaig@gmail.com>
2024-01-21 14:32:40 +0500

    repeated formats removed

--- libs/utils/date.format.js
+++ libs/utils/date.format.js
@@ -1,10 +1,8 @@
 const date_formats = [
     'MMMM Do YYYY',
-    'dddd, MMMM Do YYYY',
     'YYYY/MM/DD',
     'D MMM, YYYY',
     'dddd, MMMM Do YYYY',
-    'ddd, MMM D YYYY',
     'ddd, MMM D YYYY',
     'MMMM D, YYYY',
     'ddd, MMMM D YYYY',
